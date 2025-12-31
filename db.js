8c3df58c5290e75abab6808a60ae21ff33653c1c
Tyshchenko-Oleksandra <tyshchenko.oleksandra@lll.kpi.ua>
2025-12-31 11:11:16 +0200

    Remove commented line from db.js

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-// db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  port: 8889,          // як у твоєму PHP
+  port: 8889,         
   user: 'root',
   password: 'root',
   database: 'ucoffee_db',
